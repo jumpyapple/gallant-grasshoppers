f8db1a61c8a02c0f3973abeb69a4ea6218a21221
zach kaupp <zachkaupp@gmail.com>
2021-07-10 14:34:50 -0500

    make more flake8 approved

--- tests/test_general.py
+++ tests/test_general.py
@@ -8,7 +8,6 @@ class first_test(unittest.TestCase):
     @t.overload
     def test_stuff(self):  # type: () -> None
         """First test"""
-
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
 
