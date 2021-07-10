eee8ad06e76db628054700fa2b9f72f753d35290
zach kaupp <zachkaupp@gmail.com>
2021-07-10 14:06:47 -0500

    add tests

---
+++ tests/test_general.py
@@ -0,0 +1,8 @@
+import unittest
+
+class first_test(unittest.TestCase):
+	def test_stuff(self):
+		self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
