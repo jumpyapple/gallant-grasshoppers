ca727e9e72d9a129f299295873175f6698c55433
Tyler Richards <tyler@tyler-richards.com>
2021-07-10 17:13:43 -0400

    added rendering a border on components

--- gallant-grasshoppers/render/render.py
+++ gallant-grasshoppers/render/render.py
@@ -1,20 +1,28 @@
+from blessed import Terminal
+
+from .styles.border import draw
 
 
 class Component:
     """Components are the main way to draw to the screen of the application"""
 
     def __init__(
-            self, window: object, height: int, width: int,
-            begin_y: int, begin_x: int, data: any = None):
-        self.window = window
+            self, terminal: Terminal, window: object, width: int, height: int,
+            begin_x: int, begin_y: int, data: any = None):
+        self.terminal = terminal
         self.height = height
         self.width = width
         self.begin_y = begin_y
         self.begin_x = begin_x
         self.data = data
+        if window:
+            self.window = window
+            self.begin_x += window.begin_x
+            self.begin_y += window.begin_y
 
     def __repr__(self):
-        return self.window.move_y(self.begin_y) + self.window.move_x(self.begin_x) + str(self.data)
+        return draw(self.terminal, self) + (self.terminal.move_xy(self.begin_x + 1, self.begin_y + 1)
+                                            + str(self.data))
 
     def set_data(self, data: any = None) -> bool:
         """Sets data for to be displayed in component"""
