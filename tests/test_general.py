091c0b0ace99c08f33f353fa2f76839d77cc1018
zach kaupp <zachkaupp@gmail.com>
2021-07-10 14:24:30 -0500

    add docstrings and typing

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,8 +1,12 @@
 import unittest
+import typing as t
 
 class first_test(unittest.TestCase):
-    
+    """main test case"""
+
+    @t.overload
     def test_stuff(self):
+        """first test"""
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
 
