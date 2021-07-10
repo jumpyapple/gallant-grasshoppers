1b97fe591decaa2d86faf5a4e1fb34d25c5f5820
zach kaupp <zachkaupp@gmail.com>
2021-07-10 14:16:41 -0500

    convert indentation to spaces

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,8 +1,10 @@
 import unittest
 
 class first_test(unittest.TestCase):
-	def test_stuff(self):
-		self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    
+    def test_stuff(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
