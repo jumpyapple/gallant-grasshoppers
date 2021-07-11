a90d320da1a0c6d1938395b3105a931f43f382be
Tyler Richards <tyler@tyler-richards.com>
2021-07-11 00:18:25 -0400

    added test info into main file

--- gallant-grasshoppers/__main__.py
+++ gallant-grasshoppers/__main__.py
@@ -1,18 +1,19 @@
 from blessed import Terminal
+from render.render import Component as Comp
 
 term = Terminal()
 
 
 def main() -> None:
-    """
-    Starts up main function
-
-    :return:
-    """
+    """Starts up main function"""
     while True:
         with term.fullscreen(), term.cbreak(), term.hidden_cursor():
             print(term.home + term.clear + term.move_y(term.height // 2))
             print(term.black_on_darkkhaki(term.center('hello world')))
+            c = Comp(term, None, 20, 10, 5, 5)
+            c.set_data("Box boys: 15" + str(Comp(term, c, 12, 6, 2, 3, "Buy More?"
+                                                 + str(Comp(term, c, 3, 3, 3, 5, ">")))))
+            c.draw_component()
             term.inkey()
 
 
