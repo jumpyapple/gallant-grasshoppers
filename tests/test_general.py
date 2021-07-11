e7db1d9687e32d2beaafe4f777279df4a7e433d0
zach kaupp <zachkaupp@gmail.com>
2021-07-10 20:10:14 -0500

    take out typing module

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,11 +1,9 @@
-import typing as t
 import unittest
 
 
 class first_test(unittest.TestCase):
     """Main test case"""
 
-    @t.overload
     def test_stuff(self):  # type: () -> None
         """First test"""
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
