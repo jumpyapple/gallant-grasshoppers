0b911550348ce06bebf8b678f8a6d1fd695fa706
Tyler Richards <tyler@tyler-richards.com>
2021-07-11 22:12:36 -0400

    edit load screen code to work with components

--- gallant-grasshoppers/__main__.py
+++ gallant-grasshoppers/__main__.py
@@ -1,25 +1,27 @@
+
+
 import render as r
-# from render.component import Component
-import signal
-import time
+from render.component import Component
 
 term = r.terminal()
 
-boxer_logo = [".----------------.  .----------------.  .----------------.  .----------------.  .----------------.",
-              "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |",
-              "| |   ______     | || |     ____     | || |  ____  ____  | || |  _________   | || |  _______     | |",
-              "| |  |_   _ \\    | || |   .'    `.   | || | |_  _||_  _| | || | |_   ___  |  | || | |_   __ \\    | |",
-              "| |    | |_) |   | || |  /  .--.  \\  | || |   \\ \\  / /   | || |   | |_  \\_|  | || |   | |__) |   | |",
-              "| |    |  __'.   | || |  | |    | |  | || |    > `' <    | || |   |  _|  _   | || |   |  __ /    | |",
-              "| |   _| |__) |  | || |  \\  `--'  /  | || |  _/ /'`\\ \\_  | || |  _| |___/ |  | || |  _| |  \\ \\_  | |",
-              "| |  |_______/   | || |   `.____.'   | || | |____||____| | || | |_________|  | || | |____| |___| | |",
-              "| |              | || |              | || |              | || |              | || |              | |",
-              "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |",
-              "'----------------'  '----------------'  '----------------'  '----------------'  '----------------'"]
+boxer_logo = [
+    ".----------------.  .----------------.  .----------------.  .----------------.  .----------------.",
+    "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |",
+    "| |   ______     | || |     ____     | || |  ____  ____  | || |  _________   | || |  _______     | |",
+    "| |  |_   _ \\    | || |   .'    `.   | || | |_  _||_  _| | || | |_   ___  |  | || | |_   __ \\    | |",
+    "| |    | |_) |   | || |  /  .--.  \\  | || |   \\ \\  / /   | || |   | |_  \\_|  | || |   | |__) |   | |",
+    "| |    |  __'.   | || |  | |    | |  | || |    > `' <    | || |   |  _|  _   | || |   |  __ /    | |",
+    "| |   _| |__) |  | || |  \\  `--'  /  | || |  _/ /'`\\ \\_  | || |  _| |___/ |  | || |  _| |  \\ \\_  | |",
+    "| |  |_______/   | || |   `.____.'   | || | |____||____| | || | |_________|  | || | |____| |___| | |",
+    "| |              | || |              | || |              | || |              | || |              | |",
+    "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |",
+    "'----------------'  '----------------'  '----------------'  '----------------'  '----------------'"
+]
 
-
+"""
 # it needs to be a function for the resizing to work
-def print_start_page(sig=None, action=None):  # type () -> None
+def print_start_page(sig=None, action=None) -> None:  # type () -> None
 
     if sig is not None:
         time.sleep(.05)
@@ -30,27 +32,16 @@ def print_start_page(sig=None, action=None):  # type () -> None
     print(term.move_y(int(term.height - 1)))
     print(term.white(term.center("[PRESS SPACE TO CONTINUE]")))
     time.sleep(.05)
+"""
 
 
-# def main() -> None:
-#    """Starts up main function"""
-#    while True:
-#        with term.fullscreen(), term.cbreak(), term.hidden_cursor():
-#            print(term.home + term.clear + term.move_y(term.height // 2))
-#            print(term.black_on_darkkhaki(term.center("hello world")))
-#            c = Component(None, 20, 10, 5, 5)
-#            c.set_data("Box boys: 15" + str(Component(c, 12, 6, 2, 3, "Buy More?"))
-#                       + str(Component(c, 3, 3, 3, 5, ">")))
-#            c.draw_component()
-#            term.inkey()
-
-
-def main():  # type () -> None
+def main() -> None:
     """Starts up main function"""
     while True:
         with term.fullscreen(), term.cbreak(), term.hidden_cursor():
-            print_start_page()
-            signal.signal(signal.SIGWINCH, print_start_page)
+            print(term.home + term.clear + term.move_y(term.height // 2))
+            c = Component(None, term.width, term.height, 0, 0, boxer_logo)
+            c.draw_component()
             key_press = term.inkey()
         if key_press == " ":
             break
