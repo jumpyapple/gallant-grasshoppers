fcf9952460c577b5962834885d8466df4aeef997
zach kaupp <zachkaupp@gmail.com>
2021-07-11 19:17:21 -0500

    make flake8 accept it

--- gallant-grasshoppers/__main__.py
+++ gallant-grasshoppers/__main__.py
@@ -1,35 +1,38 @@
 import render as r
-from render.component import Component
+# from render.component import Component
 import signal
 import time
 
 term = r.terminal()
 
 boxer_logo = [".----------------.  .----------------.  .----------------.  .----------------.  .----------------.",
-"| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |",
-"| |   ______     | || |     ____     | || |  ____  ____  | || |  _________   | || |  _______     | |",
-"| |  |_   _ \\    | || |   .'    `.   | || | |_  _||_  _| | || | |_   ___  |  | || | |_   __ \\    | |",
-"| |    | |_) |   | || |  /  .--.  \\  | || |   \\ \\  / /   | || |   | |_  \\_|  | || |   | |__) |   | |",
-"| |    |  __'.   | || |  | |    | |  | || |    > `' <    | || |   |  _|  _   | || |   |  __ /    | |",
-"| |   _| |__) |  | || |  \\  `--'  /  | || |  _/ /'`\\ \\_  | || |  _| |___/ |  | || |  _| |  \\ \\_  | |",
-"| |  |_______/   | || |   `.____.'   | || | |____||____| | || | |_________|  | || | |____| |___| | |",
-"| |              | || |              | || |              | || |              | || |              | |",
-"| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |",
-"'----------------'  '----------------'  '----------------'  '----------------'  '----------------'",]
+              "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |",
+              "| |   ______     | || |     ____     | || |  ____  ____  | || |  _________   | || |  _______     | |",
+              "| |  |_   _ \\    | || |   .'    `.   | || | |_  _||_  _| | || | |_   ___  |  | || | |_   __ \\    | |",
+              "| |    | |_) |   | || |  /  .--.  \\  | || |   \\ \\  / /   | || |   | |_  \\_|  | || |   | |__) |   | |",
+              "| |    |  __'.   | || |  | |    | |  | || |    > `' <    | || |   |  _|  _   | || |   |  __ /    | |",
+              "| |   _| |__) |  | || |  \\  `--'  /  | || |  _/ /'`\\ \\_  | || |  _| |___/ |  | || |  _| |  \\ \\_  | |",
+              "| |  |_______/   | || |   `.____.'   | || | |____||____| | || | |_________|  | || | |____| |___| | |",
+              "| |              | || |              | || |              | || |              | || |              | |",
+              "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |",
+              "'----------------'  '----------------'  '----------------'  '----------------'  '----------------'"]
+
 
 # it needs to be a function for the resizing to work
-def print_start_page(sig = None, action = None):
-    if sig != None:
+def print_start_page(sig=None, action=None):  # type () -> None
+
+    if sig is not None:
         time.sleep(.05)
     print(term.home + term.clear + term.move_y(term.height // 5))
-    for i,j in enumerate(boxer_logo):
+    for i, j in enumerate(boxer_logo):
         term.move_y((term.height // 3)-i)
         print(term.sandybrown(term.center(j)))
     print(term.move_y(int(term.height - 1)))
     print(term.white(term.center("[PRESS SPACE TO CONTINUE]")))
     time.sleep(.05)
 
-#def main() -> None:
+
+# def main() -> None:
 #    """Starts up main function"""
 #    while True:
 #        with term.fullscreen(), term.cbreak(), term.hidden_cursor():
@@ -41,13 +44,17 @@ def print_start_page(sig = None, action = None):
 #            c.draw_component()
 #            term.inkey()
 
-def main(): # type () -> None
+
+def main():  # type () -> None
     """Starts up main function"""
     while True:
         with term.fullscreen(), term.cbreak(), term.hidden_cursor():
             print_start_page()
             signal.signal(signal.SIGWINCH, print_start_page)
             key_press = term.inkey()
+        if key_press == " ":
+            break
+
 
 if __name__ == '__main__':
     main()
