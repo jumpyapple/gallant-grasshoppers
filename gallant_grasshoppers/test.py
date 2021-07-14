9453b68b0acbe04e240ac2253081c63eb1e2a98d
Tyler Richards <tyler@tyler-richards.com>
2021-07-14 17:25:17 -0400

    clean up old items

--- gallant_grasshoppers/test.py
+++ gallant_grasshoppers/test.py
@@ -5,37 +5,10 @@ import views as v
 term = r.terminal()
 
 
-class StateManager:
-    """Controls current page data"""
-
-    def __init__(self, state: dict) -> None:
-        """Inits page"""
-        self.state = state
-
-    def set_state(self, state: dict) -> None:
-        """Sets current page"""
-        self.state = state
-
-    def get_state(self) -> any:
-        """Returns current page"""
-        return self.state
-
-    def get_prop(self, prop: str) -> any:
-        """Gets property information"""
-        try:
-            return self.state[prop]
-        except KeyError:
-            return False
-
-    def set_prop(self, d: tuple) -> None:
-        """Sets Property information"""
-        self.state[d[0]] = d[1]
-
-
 def main() -> None:
     """Starts up main function"""
-    c = StateManager({
-        "current_page": v.print_test_page
+    c = r.utils.StateManager({
+        "current_page": v.start_page
     })
     frame = 0
     key_press = None
